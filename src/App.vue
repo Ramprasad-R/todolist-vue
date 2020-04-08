4f8988373f8e18b52e47bab414872053e354a58d
Ramprasad-R <ramprasad.info@gmail.com>
2020-04-09 00:17:43 +0200

    get add delete todo list from jsonbin

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,52 @@
 <template>
   <div id="app">
+    <Header />
     <Todo v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <AddTodo v-on:add-todo="addTodo" />
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import Header from "./components/layout/Header";
 import Todo from "./components/Todo";
+import AddTodo from "./components/AddTodo";
 export default {
   name: "App",
   components: {
-    Todo
+    Header,
+    Todo,
+    AddTodo,
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "first todo",
-          completed: true
-        },
-        {
-          id: 2,
-          title: "second todo",
-          completed: false
-        },
-        {
-          id: 3,
-          title: "third todo",
-          completed: false
-        }
-      ]
+      todos: [],
     };
   },
   methods: {
     deleteTodo(id) {
-      console.log(id);
-    }
-  }
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(() => (this.todos = this.todos.filter((todo) => todo.id !== id)))
+        .catch((err) => console.log(err));
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed,
+        })
+        .then((res) => (this.todos = [...this.todos, res.data]))
+        .catch((err) => console.log(err));
+    },
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((res) => (this.todos = res.data))
+      .catch((err) => console.log(err));
+  },
 };
 </script>
 
@@ -49,5 +59,16 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
 }
 </style>
